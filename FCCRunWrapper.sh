adac15f95abb05f7155d459baf5732056ec74e66
Simone Bologna <smnbologna@gmail.com>
2017-04-04 16:30:45 +0100

    Updated script to be able to run FCCSW in batch mode with correct seed initialisation

--- FCCRunWrapper.sh
+++ FCCRunWrapper.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# Sandbox home directory, used to save output file
+SAVE_DEST="$(pwd)"
+
+nEvents=1
+
+while getopts "j:c:p:" o; do
+  case "${o}" in
+    j)
+      jobName=${OPTARG}
+      ;;
+    c)
+      clusterId=${OPTARG}
+      ;;
+    p)
+      processId=${OPTARG}
+      ;;
+    esac
+done
+
+randomNumberSeed=$(((clusterId+processId)%900000000))
+# Ugly workaround to setup seed
+echo "Random:seed = ${randomNumberSeed}" >> ${SAVE_DEST}/Pythia_minbias_pp_100TeV.cmd
 cd /software/sb17498/FCCSW
 source init.sh
-./run fccrun.py /software/sb17498/FCCSW/Sim/SimDelphesInterface/options/PythiaDelphes_config.py --outputfile=1event_100TeV.root --inputfile=/software/sb17498/FCCSW/Generation/data/Pythia_minbias_pp_100TeV.cmd --nevents=1
+./run fccrun.py /software/sb17498/FCCSW/Sim/SimDelphesInterface/options/PythiaDelphes_config.py --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/Pythia_minbias_pp_100TeV.cmd --nevents=${nEvents}
+#echo ${randomNumberSeed}
+#echo "${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root"

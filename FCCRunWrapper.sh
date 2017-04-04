8995bd622d858cc8736dcea8d422f34c38a87872
Simone Bologna <smnbologna@gmail.com>
2017-04-04 17:08:32 +0100

    Finished grid scripts

--- FCCRunWrapper.sh
+++ FCCRunWrapper.sh
@@ -4,7 +4,7 @@ SAVE_DEST="$(pwd)"
 
 nEvents=1
 
-while getopts "j:c:p:" o; do
+while getopts "j:c:p:i:" o; do
   case "${o}" in
     j)
       jobName=${OPTARG}
@@ -15,14 +15,17 @@ while getopts "j:c:p:" o; do
     p)
       processId=${OPTARG}
       ;;
+    i)
+      inputFile=${OPTARG}
+      ;;
     esac
 done
 
 randomNumberSeed=$(((clusterId+processId)%900000000))
 # Ugly workaround to setup seed
-echo "Random:seed = ${randomNumberSeed}" >> ${SAVE_DEST}/Pythia_minbias_pp_100TeV.cmd
+printf "\nRandom:seed = ${randomNumberSeed}\n" >> ${SAVE_DEST}/${inputFile}
 cd /software/sb17498/FCCSW
 source init.sh
-./run fccrun.py /software/sb17498/FCCSW/Sim/SimDelphesInterface/options/PythiaDelphes_config.py --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/Pythia_minbias_pp_100TeV.cmd --nevents=${nEvents}
+./run fccrun.py /software/sb17498/FCCSW/Sim/SimDelphesInterface/options/PythiaDelphes_config.py --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/${inputFile} --nevents=${nEvents}
 #echo ${randomNumberSeed}
 #echo "${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root"

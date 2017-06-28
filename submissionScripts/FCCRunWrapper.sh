cc134029e6e559afeb4de873225ce4b1917f867f
Simone Bologna <smnbologna@gmail.com>
2017-06-28 14:06:24 +0100

    Improving batch submission techinique

--- submissionScripts/FCCRunWrapper.sh
+++ submissionScripts/FCCRunWrapper.sh
@@ -5,11 +5,11 @@ SAVE_DEST="$(pwd)"
 nEvents=5000
 
 # Destination in /HDFS/FCC-hh
-HDFS_DEST="HardQCD_PtBinned_700_900_GeV"
 inputFile="Pythia_HardQCD_700_900.cmd"
-software="/software/sb17498/FCCSW/Generation/data"
+software="/software/sb17498/FCCSW/"
+config="Sim/SimDelphesInterface/options/PythiaDelphes_config_CMS.py"
 
-while getopts "j:c:p:i:n:" o; do
+while getopts "j:c:p:i:n:s:d:" o; do
   case "${o}" in
     j)
       jobName=${OPTARG}
@@ -26,21 +26,26 @@ while getopts "j:c:p:i:n:" o; do
     n) 
       nEvents=${OPTARG}
       ;;
+    s)
+      config=${OPTARG}
+      ;;
+    d)
+      HDFS_DEST=${OPTARG}
+      ;;
     esac
 done
 
-cp ${software}/${inputFile} ${SAVE_DEST}/${inputFile}
+cp ${software}Generation/data/${inputFile} ${SAVE_DEST}/${inputFile}
 
 randomNumberSeed=$(((clusterId+processId)%900000000))
 # Ugly workaround to setup seed
 printf "\nRandom:seed = ${randomNumberSeed}\n" >> ${SAVE_DEST}/${inputFile}
 cd /software/sb17498/FCCSW
 source init.sh
-./run fccrun.py /software/sb17498/FCCSW/Sim/SimDelphesInterface/options/PythiaDelphes_config_CMS.py --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/${inputFile} --nevents=${nEvents}
+set -o xtrace
+./run fccrun.py ${software}${config} --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/${inputFile} --nevents=${nEvents}
 #echo ${randomNumberSeed}
 echo "${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root"
-
-set -o xtrace
 
 /usr/bin/hdfs dfs -mkdir -p /FCC-hh/${HDFS_DEST}
 /usr/bin/hdfs dfs -moveFromLocal ${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root /FCC-hh/${HDFS_DEST}

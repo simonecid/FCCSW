0ec1910fa7ff0081100ee99316455239e26f1b04
Simone Bologna <smnbologna@gmail.com>
2018-02-01 13:38:22 +0000

    F.A.C.K.

--- init_localDelphes.sh
+++ init_localDelphes.sh
@@ -5,4 +5,4 @@ export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 weekday=`date +%a`
 source /cvmfs/fcc.cern.ch/testing/sw/views/stable/x86_64-slc6-gcc62-opt/setup.sh
 export DELPHES_DIR=/storage/sb17498/FCC/Delphes_install
-export CMAKE_PREFIX_PATH=/storage/sb17498/FCC/FCCSW/Delphes_install:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=/storage/sb17498/FCC/Delphes_install:$CMAKE_PREFIX_PATH

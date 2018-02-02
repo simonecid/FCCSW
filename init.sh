0c45993cdc125d9a8b8a69c37c58fe3b629f0bd1
Simone Bologna <smnbologna@gmail.com>
2018-02-02 17:41:32 +0000

    Fixed init

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 weekday=`date +%a`
 source /cvmfs/fcc.cern.ch/testing/sw/views/stable/x86_64-slc6-gcc62-opt/setup.sh
-export DELPHES_DIR=../Delphes_install
-export CMAKE_PREFIX_PATH=../Delphes_install:$CMAKE_PREFIX_PATH
+export DELPHES_DIR=/software/sb17498/Delphes_install
+export CMAKE_PREFIX_PATH=/software/sb17498/Delphes_install:$CMAKE_PREFIX_PATH

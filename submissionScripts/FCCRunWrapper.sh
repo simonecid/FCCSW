3875587206c5079bec401931ceb73e5b5270a89b
Simone Bologna <smnbologna@gmail.com>
2018-05-22 09:24:20 +0100

    Updated wrapper

--- submissionScripts/FCCRunWrapper.sh
+++ submissionScripts/FCCRunWrapper.sh
@@ -37,20 +37,23 @@ done
 
 set -o xtrace
 
+git clone https://www.github.com/simonecid/FCCSW
+
+cd FCCSW
+set +o xtrace
+source init.sh
+set -o xtrace
+make -j4
+
 # We have to setup the seed, therefore we create an individual copy of the pythia cmd and we will append its seed to it
-cp ${software}/Generation/data/${inputFile} ${SAVE_DEST}/${inputFile}
 randomNumberSeed=$(((clusterId+processId)%900000000))
-printf "\nRandom:seed = ${randomNumberSeed}\n" >> ${SAVE_DEST}/${inputFile}
+printf "\nRandom:seed = ${randomNumberSeed}\n" >> Generation/data/${inputFile}
 printf "\nRandom:seed = ${randomNumberSeed}\n"
 
 echo "I am running on" $HOSTNAME
 
 # Running the sim
-set +o xtrace
-cd /software/sb17498/FCCSW
-source init.sh
-set -o xtrace
-./run fccrun.py ${software}/${config} --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=${SAVE_DEST}/${inputFile} --nevents=${nEvents}
+./run fccrun.py ${software}/${config} --outputfile=${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root --inputfile=Generation/data/${inputFile} --nevents=${nEvents}
 echo "${SAVE_DEST}/events_${jobName}_${clusterId}.${processId}.root"
 
 # Copying output
